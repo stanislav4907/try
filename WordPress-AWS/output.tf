63e31d94bf7b9a252d61da01e55e8916c32bdae0
EC2 Default User <ec2-user@ip-172-31-16-45.eu-west-1.compute.internal>
2023-03-24 05:32:21 +0000

    added RDS

---
+++ WordPress-AWS/output.tf
@@ -0,0 +1,16 @@
+output "address" {
+  value = aws_db_instance.default.address
+}
+output "availability_zone" {
+  value = aws_db_instance.default.availability_zone
+}
+output "engine" {
+  value = aws_db_instance.default.engine
+}
+output "id" {
+  value = aws_db_instance.default.id
+}
+
+output "port" {
+  value = aws_db_instance.default.port
+}
